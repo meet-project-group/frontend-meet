985e2a6916eb220e0cbb479c0767d589917f9bef
kevinseya17 <165537606+kevinseya17@users.noreply.github.com>
2025-12-11 05:19:40 -0500

    Update voiceSocket.ts

--- src/services/voiceSocket.ts
+++ src/services/voiceSocket.ts
@@ -2,6 +2,6 @@ import { io } from "socket.io-client";
 
 export const voiceSocket = io(import.meta.env.VITE_VOICE_SERVER_URL, {
   autoConnect: false,
-  transports: ["websocket"],  // ← OBLIGATORIO
-  path: "/voice/socket.io"    // ← tu backend sí usa este path
+  transports: ["websocket"],
+  path: "/voice/socket.io",
 });
