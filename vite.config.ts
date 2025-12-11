7af1805f6daf972160ef9699aa67fe5737ad3dfb
kevinseya17 <165537606+kevinseya17@users.noreply.github.com>
2025-12-11 14:54:31 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
 export default defineConfig({
   plugins: [react()],
-  base: "",
+  base: "/", // ← Importante PARA PRODUCCIÓN
   optimizeDeps: {
-    include: ["peerjs"]
-  }
+    include: ["peerjs"],
+  },
+  build: {
+    outDir: "dist",
+  },
 });
 
