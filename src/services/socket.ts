62ee425b788f6257bf640abb95c6c74a53d52e84
kevinseya17 <165537606+kevinseya17@users.noreply.github.com>
2025-12-11 06:51:22 -0500

    Update socket.ts

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -1,5 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:4000", {
-  autoConnect: true
+export const socket = io(import.meta.env.VITE_CHAT_SERVER_URL, {
+  transports: ["websocket"],
+  autoConnect: true,
 });
