fad1df77e7ee03061c8d4395ee8998f893a35114
kevinseya17 <165537606+kevinseya17@users.noreply.github.com>
2025-12-15 11:02:41 -0500

    Update room.tsx

--- src/pages/room.tsx
+++ src/pages/room.tsx
@@ -1,4 +1,4 @@
-iimport { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 
 import "../styles/room.sass";
